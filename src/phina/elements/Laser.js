6810f09a2c73e6d3cb443caefeb4c42b5e7d3190
minimo <minimo.mobile@gmail.com>
2021-08-16 16:49:44 +0900

    レーザー残像追加

--- src/phina/elements/Laser.js
+++ src/phina/elements/Laser.js
@@ -1,20 +1,20 @@
 import {GameObject} from "@/phina/elements/GameObject";
-import {RectangleShape} from "phina.js";
+import {RectangleShape, Vector2} from "phina.js";
 
 export class Laser extends GameObject {
+  static rectOptions = {
+    width: 3,
+    height: 1000,
+    fill: 'blue',
+    stroke: 'blue',
+    strokeWidth: 1,
+    cornerRadius: 0,
+  };
 
   constructor(options) {
     super(options);
 
-    const rectOptions = {
-      width: 3,
-      height: 1000,
-      fill: 'blue',
-      stroke: 'blue',
-      strokeWidth: 1,
-      cornerRadius: 0,
-    }
-    this.sprite = new RectangleShape(rectOptions)
+    this.sprite = new RectangleShape(Laser.rectOptions)
       .setPosition(0, -6)
       .setOrigin(0.5, 1.0)
       .addChildTo(this)
@@ -29,15 +29,44 @@ export class Laser extends GameObject {
      * @type {number}
      */
     this.lifeSpan = 10;
+
+    /**
+     * 前フレームの座標
+     * @type {Vector2}
+     */
+    this.beforePosition = new Vector2();
+
+    /**
+     * 前フレームの角度
+     * @type {number}
+     */
+    this.beforeRotation = 0;
   }
 
   update() {
     if (this.time > this.lifeSpan) this.remove();
     if (this.shooter) {
-      // const bp = this.position.clone();
       this.setPosition(this.shooter.x, this.shooter.y);
-      this.rotation = this.shooter.angle * 22.5;
+      this.setRotation(this.shooter.angle * 22.5);
+      this.afterImage();
     }
+    this.beforePosition.set(this.position.x, this.position.y);
+    this.beforeRotation = this.rotation;
+  }
+
+  afterImage() {
+    if (!this.parent) return;
+    const image = new RectangleShape(Laser.rectOptions)
+      .setPosition(this.beforePosition.x, this.beforePosition.y)
+      .setRotation(this.beforeRotation)
+      .setOrigin(0.5, 1.0)
+      .addChildTo(this.parent);
+    image.lifeSpan = 5;
+    image.on('enterframe', () => {
+      image.lifeSpan--;
+      image.alpha -= 0.2;
+      if (image.lifeSpan < 0) image.remove();
+    });
   }
 
   destroy() {
